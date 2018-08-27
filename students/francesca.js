d9c920ff57dea8096f96bfafe586f1f4f0eb7882
Francesca Ropolo <franci.ropolo@gmail.com>
2018-08-27 22:33:51 +0200

    now it works buuut still doesnt with arrows

--- students/francesca.js
+++ students/francesca.js
@@ -1,20 +1,20 @@
 'use stinct'
 
+  var nodes = document.querySelectorAll('article h4');
 
-  var nodes = document.querySelectorAll('desplegable');
-  
-  for (var ix = 0; ix < nodes.length; ix++) {
-    nodes[ix].addEventListener('click', function (event) {
-      var h4 = event.currentTarget;
-      var article = h4.parentNode;
-      var div = article.querySelectorAll('div');
-      div.classList.toggle('expanded');
-      var arrowDown = h4.firstElementChild;
-      var arrowUP = h4.lastElementChild; //why not .children
-      arrowUp.classList.toggle('expanded');
-      arrowDown.classList.toggle('expanded');
-    })
+  function main() {
+    for (var ix = 0; ix < nodes.length; ix++) {
+      nodes[ix].addEventListener('click', function (event) {
+        var h4 = event.currentTarget;
+        var article = h4.parentNode;
+        var div = article.querySelector('div');
+        div.classList.toggle('expanded');
+        // var arrowDown = h4.firstElementChild;
+        // var arrowUP = h4.lastElementChild; //why not .children
+        // arrowUp.classList.toggle('expand');
+        // arrowDown.classList.toggle('hide');
+      })
+    }
   };
 
-
-
+  window.addEventListener('load', main);
