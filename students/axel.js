af37c390670d8a3d3f6f77b26577db9679e91007
Axel Garcia <axel.garcia94@gmail.com>
2018-08-28 20:21:48 +0200

    added more DOM manipulation

--- students/axel.js
+++ students/axel.js
@@ -3,6 +3,8 @@
 function main() {
 
 
+//-----EXPERIMENTS----//
+
   var nodes1 = document.querySelectorAll('.list-articles h4');
   for (var ix = 0; ix < nodes1.length; ix++) {
     nodes1[ix].addEventListener('click', function (event) {
@@ -14,6 +16,8 @@ function main() {
   };
 
 
+//-----BUTTON-----//
+
   var mainButton = document.querySelector('.site-nav .button');
     mainButton.addEventListener('mouseover', function (event) {
       event.target.classList.toggle('button-inverted');
@@ -21,8 +25,6 @@ function main() {
     mainButton.addEventListener('mouseout', function (event) {
       event.target.classList.toggle('button-inverted');
     });
-
-
 
   var secondaryButton = document.querySelectorAll('.list-articles a');
   for (var ix = 0; ix < secondaryButton.length; ix++) {
@@ -34,6 +36,8 @@ function main() {
     });
   };
 
+
+//----DROP DOWN MENU----//
 
   function findStudents(term){
     var results = [];
@@ -48,11 +52,8 @@ function main() {
     return results;
   };
 
-
   function displayResults(results){
-    var searchResult = document.querySelector('.search-results');
     searchResult.innerHTML = '';
-
     var ul = document.createElement('ul');
     results.forEach(function (student) {
       var link = document.createElement('a');
@@ -65,18 +66,56 @@ function main() {
     searchResult.appendChild(ul);
   };
 
-
   function handleChange (){
     var searchTerms = input.value.toUpperCase();
     var results = findStudents(searchTerms);
     displayResults(results);
   };
 
-
-
   var input = document.querySelector('.input-student input');
   input.addEventListener('keyup', handleChange);
+
+  var searchResult = document.querySelector('.search-results'); 
+  document.body.addEventListener('click', function (){
+    searchResult.innerHTML = ''
+  });
+
+  document.querySelector('.input-student input').addEventListener('click', function (event){
+    event.stopPropagation(event);
+  });
+
+  document.body.addEventListener('keyup', function (){
+    if (event.key === 'Escape') {
+      searchResult.innerHTML = '' 
+    };
+  });
+
+  document.querySelector('.input-student input').addEventListener('focus', handleChange);
+
+
+
+
+//-----TIMER-----//
+
+  var time = 30;
+  var p = document.createElement('p');
+  p.classList.add('button');
+  p.classList.add('timer');
+  p.innerText = time;
+  document.querySelector('.main-image').appendChild(p);
+  var timerId = setInterval(function () {
+      time -- ;
+      if (time === 0){
+        clearInterval(timerId);
+      };
+      p.innerText = time;
+      document.querySelector('.timer').addEventListener('click', function (){
+        clearInterval(timerId); 
+    });
+  }, 1000);
 };
+
+
 
 
 window.addEventListener('load', main);
