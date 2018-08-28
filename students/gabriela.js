eabdeabb6c3c095926e1dfde3db6fd87cbe4216e
Gabriela Arias <gap1885@gmail.com>
2018-08-28 11:47:20 +0200

    corregido otra vez Gab

--- students/gabriela.js
+++ students/gabriela.js
@@ -4,7 +4,9 @@ function main() {
   var nodes = document.querySelectorAll('section h2');
    for (var ix = 0 ; ix < nodes.length; ix++) {
     nodes[ix].addEventListener('click', function (event) {
-      var h2 = event.currentTarget;
+      
+      
+  var h2 = event.currentTarget;
       var section = h2.parentNode;
       section.classList.toggle('expanded');
     });
