dd57585b80c2a57beb17e3466c4537cae1f0f5af
Francesca Ropolo <franci.ropolo@gmail.com>
2018-08-28 16:59:05 +0200

    quick jump search

--- students/francesca.js
+++ students/francesca.js
@@ -1,4 +1,4 @@
-'use strinct'
+'use strict'
 
   var nodes = document.querySelectorAll('article h4');
 
@@ -10,6 +10,55 @@
         article.classList.toggle('is-expanded')
       })
     }
+
+    var input = document.querySelector('.sub-header input');
+    input.addEventListener('keyup', handleChange);
+
+    function findStudents(terms) {
+      var results = [];
+
+      if (terms) {
+        results = students.filter( function(student) {
+          return student.name.toLowerCase().indexOf(terms) >= 0;
+        });
+      };
+      return results;
+    };
+
+    function displayResults(results) {
+      var searchResults =  document.querySelector('.sub-header .search-results');
+      searchResults.innerHTML = '';
+
+      if (!results.length) {
+        return;
+      }
+      
+      var ul = document.createElement('ul');
+      
+      results.forEach(function (student) {
+        var link = document.createElement('a');
+        
+        var li = document.createElement('li');
+
+        link.innerText = student.name;
+        link.setAttribute('href', '../' + student.url);
+        
+        li.appendChild(link);
+        ul.appendChild(li);
+      });
+
+      searchResults.appendChild(ul);
+
+    };
+
+    function handleChange() {
+      var searchTerms = input.value;
+      var results = findStudents(searchTerms);
+      
+      displayResults(results)
+      
+    };
+
   };
 
   window.addEventListener('load', main);
