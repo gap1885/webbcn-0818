706d027231afecbd2c058d7a36358e456bb16f02
Gabriela Arias <gap1885@gmail.com>
2018-08-28 07:50:05 +0200

    Gabriela

---
+++ students/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function main() {
+  var nodes = document.querySelectorAll('h2');
+  
+  for (var ix = 0; ix < nodes.length; ix++) {
+    nodes[ix].addEventListener('click', function (event) {
+      var h2 = event.currentTarget;
+      var section = h2.parentNode;
+      section.classList.toggle('expanded');
+    });
+  };
+}
+window.addEventListener('load', main);
