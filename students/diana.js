7a92f0565c6d94fa2466e486d4e6b9b9649a460a
Diana Blanco <dbp@Dianas-MacBook-Pro.local>
2018-08-28 11:17:33 +0200

    add toggle

--- students/diana.js
+++ students/diana.js
@@ -1,14 +1,18 @@
 'use strict';
 
 console.log("Hello");
+function experiments() {
 
-var nodes = document.querySelectorAll('section.experiments h3');
+  var nodes = document.querySelectorAll('section.experiments h3');
 
-for (var ix = 0; ix < nodes.length; ix++) {
-nodes[ix].addEventListener('click', function (event) {
-  var h3 = event.currentTarget;
-  var div = h3.parentNode;
-  var article = article.querySelector('.article');
-  article.classList.toogle('hidden')
-});
+  for (var ix = 0; ix < nodes.length; ix++) {
+    nodes[ix].addEventListener('click', function (event) {
+      var h3 = event.currentTarget;
+      var div = h3.parentNode;
+      var article = div.querySelector('article');
+      article.classList.toggle('hidden')
+  });
+  }
 }
+
+window.addEventListener('load',experiments)
