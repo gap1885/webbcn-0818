0c4bdece264043ac3f92b8e1d73517ccd6652443
Barbara Koehler <barkoehler@gmail.com>
2018-08-28 13:59:54 -0400

    website with hidden search results and timer

--- students/barbara.js
+++ students/barbara.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 function main() {
   var nodes = document.querySelectorAll('section h2');
   
@@ -11,6 +10,92 @@ function main() {
       section.classList.toggle('expanded');
     });
   };
-}
+
+  function findStudents(terms) {
+    var results = [];
+    
+    if (terms) {
+      results = students.filter(function (student) {
+        return student.name.toLowerCase().indexOf(terms) >= 0;
+      });
+    };
+    return results;
+  };
+
+  function displayResults(results) {
+    var searchResults = document.querySelector('.quick-nav .search-results');
+    searchResults.innerHTML = '';
+
+    var ul = document.createElement('ul');
+    results.forEach(function (student) {
+      var link = document.createElement('a');
+      link.innerText = student.name;
+      link.setAttribute('href', '../' + student.url);
+
+      var li = document.createElement('li');
+      li.appendChild(link);
+      ul.appendChild(li);
+    });
+    searchResults.appendChild(ul);
+  };
+
+  function handleChange() {
+    var searchTerms = input.value.toLowerCase();
+    var results = findStudents(searchTerms);
+    displayResults(results);
+  };
+
+  var input = document.querySelector('.quick-nav .search input');
+  input.addEventListener('keyup', handleChange);
+
+  var searchResults = document.querySelector('.quick-nav .search-results');
+
+  document.body.addEventListener('click', function() {
+    searchResults.innerHTML = '';
+  });
+
+  input.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
+
+  input.addEventListener('focus', handleChange);
+
+  document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 27) { 
+      searchResults.innerHTML = '';
+   } 
+});
+};
 
 window.addEventListener('load', main);
+
+
+// Async challenge
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  
+  return {
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
+
