04ba93c8c348cc048e75875d4b17e8fcc4b8daff
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2018-08-28 11:55:06 +0200

    add students data

---
+++ students.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var students = [{
+  url: 'students/axel.html',
+  name: 'Axel',
+  country: 'España'
+},{
+  url: 'students/barbara.html',
+  name: 'Barbara',
+  country: 'Deutschland'
+},{
+  url: 'students/caroline.html',
+  name: 'Caroline',
+  country: 'Brasil'
+},{
+  url: 'students/diana.html',
+  name: 'Diana',
+  country: 'Colombia'
+},{
+  url: 'students/francesca.html',
+  name: 'Francesca',
+  country: 'Italia'
+},{
+  url: 'students/gabriela.html',
+  name: 'Gabriela',
+  country: 'Ecuador'
+},{
+  url: 'students/iban.html',
+  name: 'Iban',
+  country: 'España'
+},{
+  url: 'students/jonathan.html',
+  name: 'Jonathan',
+  country: 'Deutschland'
+},{
+  url: 'students/maria-jose.html',
+  name: 'Maria José',
+  country: 'España'
+},{
+  url: 'students/mireia.html',
+  name: 'Mireia',
+  country: 'España'
+},{
+  url: 'students/yenderly.html',
+  name: 'Yenderly',
+  country: 'Venezuela'
+}];
