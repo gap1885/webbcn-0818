c1d9f7c16e5745043200536d3f04fd5d6d38916e
jsplmns <jonathan@spielmannsbilder.de>
2018-08-28 08:14:24 +0200

    js added FAIL BCN Jonathan

---
+++ students/jonathan.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// function main() {
+//   var nodes = document.querySelectorAll('nav h3');
+  
+//   for (var ix = 0; ix < nodes.length; ix++) {
+//     nodes[ix].addEventListener('click', function (event) {
+//       console.log('Hola Johnny');
+//       var h3 = event.currentTarget;
+//       var nav = h3.parentNode;
+//       // var list = nav.querySelector('ul')
+//       nav.classList.toggle('hidden');
+//     });
+//   };
+// }
+
+// document.addEventListener('load', main);
+
+function navButton() {
+  var nodes = document.getElementById('navButton');
+
+  for (var ix = 0; ix < nodes.length; ix++) {
+    nodes[ix].addEventListener('click', function (event) {
+      console.log('jonathan king');
+      var h3 = event.currentTarget;
+      var nav = h3.parentNode;
+      // var list = nav.querySelector('ul')
+      nav.classList.toggle('hidden');
+    });
+  };
+}
+
+document.addEventListener('load', navButton);
