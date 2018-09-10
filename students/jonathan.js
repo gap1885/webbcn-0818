0fd7fef56556980b54b0db08beeb0fc1fe251c0c
jsplmns <jonathan@spielmannsbilder.de>
2018-09-10 16:32:22 +0200

    update to ES6

--- students/jonathan.js
+++ students/jonathan.js
@@ -1,37 +1,34 @@
 'use strict';
 
 
-function main() {
+const main = () => {
 
-  var navButton = document.getElementById('nav-button');
+  const navButton = document.getElementById('nav-button');
 
-  navButton.addEventListener('click', function (event) {
-    var h3 = event.currentTarget;
-    var nav = h3.parentNode;
+  navButton.addEventListener('click', (event) => {
+    const h3 = event.currentTarget;
+    const nav = h3.parentNode;
     nav.classList.toggle('hidden');
   });
 
 
-  var nodes = document.querySelectorAll('article.card h3');
+  const nodes = document.querySelectorAll('article.card h3');
   
-  for (var ix = 0; ix < nodes.length; ix++) {
-    nodes[ix].addEventListener('click', function (event) {
-      var h3 = event.currentTarget;
-      var article = h3.parentNode;
+  for (let ix = 0; ix < nodes.length; ix++) {
+    nodes[ix].addEventListener('click', (event) => {
+      const h3 = event.currentTarget;
+      const article = h3.parentNode;
       article.classList.toggle('hidden');
     });
   };
 
 
-  var input = document.querySelector('#quick-jump')
-
-  input.addEventListener('keyup', handleChange)
-
-  function findStudents(terms) {
-    var results = [];
-
+  
+  const findStudents = (terms) => {
+    let results = [];
+    
     if (terms) {
-      var results = students.filter(function(item){
+      results = students.filter(item => {
         if (item.name.toLowerCase().indexOf(terms) !== -1) {
           return true;
         }; if (item.name.indexOf(terms) !== -1) {
@@ -42,58 +39,55 @@ function main() {
     };
     return results;
   }
-
-  function displayResults(results) {
-    var searchResults = document.querySelector('.quick-nav .search-results');
+  
+  const displayResults = (results) => {
+    const searchResults = document.querySelector('.quick-nav .search-results');
     searchResults.innerHTML = '';
     
-    var ul = document.createElement('ul');
-    results.forEach(function (student) {
-      var link = document.createElement('a');
+    const ul = document.createElement('ul');
+    results.forEach((student) => {
+      const link = document.createElement('a');
       link.innerText = student.name;
       link.setAttribute('href', '../' + student.url);
-
-      var li = document.createElement('li');
+      
+      const li = document.createElement('li');
       li.appendChild(link);
-
+      
       ul.appendChild(li);
     });   
     searchResults.appendChild(ul);
   }
-
-  function handleChange() {
-    var searchTerms = input.value;
-    var results = findStudents(searchTerms);
+  
+  const handleChange = () => {
+    let searchTerms = input.value;
+    let results = findStudents(searchTerms);
     displayResults(results);
-
-    var searchResults = document.querySelector('.quick-nav .search-results');
-    document.body.addEventListener('click', function() {
-      searchResults.innerHTML = '';
-    })
-
-    document.addEventListener('keyup', function(e) {
+    
+    const searchResults = document.querySelector('.quick-nav .search-results');
+    document.body.addEventListener('click', () => searchResults.innerHTML = '')
+    
+    document.addEventListener('keyup', (e) => {
       if (e.keyCode == 27) { 
         searchResults.innerHTML = '';
       }
     });
-
-    input.addEventListener('click', function(event) {
-      event.stopPropagation();
-    });
-
-
-
+    
+    input.addEventListener('click', (event) => event.stopPropagation())
+    
     input.addEventListener('focus', handleChange);
-
-
+    
+    
     console.log(results);
   }
+  
+  const input = document.querySelector('#quick-jump')
 
-
+  input.addEventListener('keyup', handleChange)
+  
   // ----------------------------------------
   // ----------- GAME COUNTDOWN -------------
   // ----------------------------------------
-
+  
 
 
   // function countDown(i) {
@@ -105,40 +99,35 @@ function main() {
   // }
   // countDown(30);
 
-  var playButton = document.querySelector('.game .button');
+  const playButton = document.querySelector('.game .button');
   
-  playButton.addEventListener('click', countDown);
-
-  function countDown(counter) {
+  
+  const countDown = () => {
     playButton.removeEventListener('click', countDown);
-
-    var description = document.querySelector('.game .description');
+    
+    const description = document.querySelector('.game .description');
     description.innerHTML = "find the easter egg on this page and click it!";
     
-    var easterEgg = document.querySelector('section.overview img')
-    easterEgg.addEventListener('click', function() {
-      // var displayCountdown = document.querySelector('.game .counter-box');
-      clickedImage = true;
-      // displayCountdown.innerHTML = '0';
-    });
-
-    var clickedImage = false;
-
-    var counter = 30;
-    var timeoutId = window.setInterval(function(){
+    const easterEgg = document.querySelector('section.overview img')
+    easterEgg.addEventListener('click', () => clickedImage = true)
+    
+    let clickedImage = false;
+    
+    let counter = 30;
+    const timeoutId = window.setInterval(() => {
       if (clickedImage) {
         clearInterval(timeoutId);
       } else if (counter >= 0) {
-        var displayCountdown = document.querySelector('.game .counter-box');
+        const displayCountdown = document.querySelector('.game .counter-box');
         displayCountdown.innerHTML = counter;
         counter--;
-        // console.log(counter);
       } else {
         clearInterval(timeoutId);
       };
     }, 100);
   };
-
+  
+  playButton.addEventListener('click', countDown);
 }
 
 window.addEventListener('load', main);
